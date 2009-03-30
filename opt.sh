87aa05f90dcdf8a00fd05828fdceef968a1ca68b
jdh302 <jdh302@2cf9bd7f-d7d4-4eed-827f-6c2379e778c9>
2009-03-30 21:24:12 +0000

    Authors file. Compiled with profiling.
    git-svn-id: svn://svn.forge.ocamlcore.org/svn/hlvm@18 2cf9bd7f-d7d4-4eed-827f-6c2379e778c9

--- opt.sh
+++ opt.sh
@@ -1,5 +1,6 @@
-llc -f aout.bc -o aout.s
+llc -tailcallopt -f aout.bc -o aout.s
 gcc -lsigsegv -ldl -lm aout.s -o aout
+gcc -p -lsigsegv -ldl -lm aout.s -o aoutprof
 opt -std-compile-opts <aout.bc >aoutopt.bc
-llc -f aoutopt.bc -o aoutopt.s
+llc -tailcallopt -f aoutopt.bc -o aoutopt.s
 gcc -lsigsegv -ldl -lm aoutopt.s -o aoutopt
