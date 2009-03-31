7c25dd4b7a319d470f24d7f57110921e9f24e25e
jdh302 <jdh302@2cf9bd7f-d7d4-4eed-827f-6c2379e778c9>
2009-03-31 00:52:28 +0000

    Improved top-level.
    git-svn-id: svn://svn.forge.ocamlcore.org/svn/hlvm@19 2cf9bd7f-d7d4-4eed-827f-6c2379e778c9

--- toplevel.ml
+++ toplevel.ml
@@ -3,42 +3,68 @@ open Expr
 open Camlp4.PreCast
 
 let expr = Gram.Entry.mk "expr"
+let param_list = Gram.Entry.mk "param_list"
+let str_item = Gram.Entry.mk "str_item"
 
 EXTEND Gram
   expr:
-  [ "sum" LEFTA
-      [ e1 = expr; "+"; e2 = expr -> e1 +: e2
-      | e1 = expr; "-"; e2 = expr -> e1 -: e2 ]
-  | "product" LEFTA
-      [ e1 = expr; "*"; e2 = expr -> e1 *: e2
-      | e1 = expr; "/"; e2 = expr -> e1 /: e2 ]
-  | "simple" NONA
-      [ n = INT -> Int(int_of_string n)
-      | x = FLOAT -> Float(float_of_string x)
-      | "("; e = expr; ")" -> e ] ];
-  END
+     [ "apply" LEFTA
+	 [ f = expr; "(" ; args = LIST0 expr SEP "," ; ")" ->
+	     Apply(f, args) ]
+     | "sum" LEFTA
+	 [ e1 = expr; "+"; e2 = expr -> e1 +: e2
+	 | e1 = expr; "-"; e2 = expr -> e1 -: e2 ]
+     | "product" LEFTA
+	 [ e1 = expr; "*"; e2 = expr -> e1 *: e2
+	 | e1 = expr; "%"; e2 = expr -> e1 %: e2
+	 | e1 = expr; "/"; e2 = expr -> e1 /: e2 ]
+     | "logic" LEFTA
+	 [ e1 = expr; "&&"; e2 = expr -> e1 &&: e2
+	 | e1 = expr; "||"; e2 = expr -> e1 ||: e2 ]
+     | "cmp" LEFTA
+	 [ e1 = expr; "<"; e2 = expr -> e1 <: e2
+	 | e1 = expr; "<="; e2 = expr -> e1 <=: e2
+	 | e1 = expr; "="; e2 = expr -> e1 =: e2
+	 | e1 = expr; "<>"; e2 = expr -> e1 <>: e2
+	 | e1 = expr; ">="; e2 = expr -> e1 >=: e2
+	 | e1 = expr; ">"; e2 = expr -> e1 >: e2 ]
+     | [ "if"; p = expr; "then"; t = expr; "else"; f = expr ->
+	   If(p, t, f) ]
+     | "simple" NONA
+	 [ n = INT -> Int(int_of_string n)
+	 | x = FLOAT -> Float(float_of_string x)
+	 | "("; e = expr; ")" -> e 
+	 | v = LIDENT -> Var v ]
+     ];
+  param_list:
+    [ [ "(" ; args = LIST1 [ v = LIDENT -> v ] SEP "," ; ")" ->
+	  List.map (fun x -> x, `Float) args ]
+    | [ x = LIDENT -> [x, `Float] ]
+    ];
+  str_item:
+    [ [ "let" ; f = LIDENT ; params = param_list;  "=" ; body = expr; ";;" ->
+          (`Function(f, params, `Float, body) : Hlvm.t) ]
+    | [ e = expr; ";;"  -> `Expr e ]
+    ];
+END
 
 let try_input_line ch =
   try Some(input_line ch) with End_of_file -> None
 
 let rec toploop() =
   printf "> %!";
-  let line = try_input_line stdin in
-  match line with
-  | None ->
-      printf "\n%!";
-  | Some string ->
-      begin
-	try
-	  let expr = Gram.parse expr Loc.ghost (Stream.of_string string) in
-	  try
-	    Hlvm.eval (`Expr expr)
-	  with exn ->
-	    printf "Eval error: %s\n%!" (Printexc.to_string exn)
-	with exn ->
-	  printf "Parse error: %s\n%!" (Printexc.to_string exn)
-      end;
-      toploop()
+  begin
+    try
+      let func_def =
+	Gram.parse str_item Loc.ghost (Stream.of_channel stdin) in
+      try
+	Hlvm.eval func_def
+      with exn ->
+	printf "Eval error: %s\n%!" (Printexc.to_string exn)
+    with exn ->
+      printf "Parse error: %s\n%!" (Printexc.to_string exn)
+  end;
+  toploop()
 
 let () =
   toploop()
